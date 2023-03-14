dd39f73f0b6e7a043f9e4888123f77503688bd61
miguel.sdela <miguel.sdela@babelgroup.com>
2023-03-14 11:33:18 +0100

    Primer commit

---
+++ src/main/java/com/babel/PolizaMyBatis/PolizaMyBatisApplication.java
@@ -0,0 +1,13 @@
+package com.babel.PolizaMyBatis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolizaMyBatisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PolizaMyBatisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/babel/PolizaMyBatis/PolizaMyBatisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.babel.PolizaMyBatis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolizaMyBatisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
